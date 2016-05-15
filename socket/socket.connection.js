b8df3e1f5fd3ae83a1db3ed22d0fc6b6e9cf8ddc
maoz8153 <maoz8153@gmail.com>
2016-05-16 00:43:00 +0300

    change connection

--- socket/socket.connection.js
+++ socket/socket.connection.js
@@ -5,26 +5,27 @@ var io = require('../bin/www');
 var Q = require("q");
 
 var sokcetFunction = function (socket) {
-  socket.on('send message', function (message) {
+    socket.on('send message', function (message) {
         Message.newMassege(message).then(function (data) {
-          console.log(data);
-          io.sockets.emit('new message', data);
+            console.log(data);
+            message.date = data.created_at;
+            io.sockets.emit('new message', message);
         })
-        .catch(function (error) {
+            .catch(function (error) {
                 socket.emit('error', error);
-        });
+            });
 
     });
 
     socket.on('new user', function (user, index) {
 
-          User.createUser(user).then(function (data) {
+        User.createUser(user).then(function (data) {
             console.log(data);
             socket.emit('new user confirm', data);
-          })
-          .catch(function (error) {
-            socket.emit('error', error);
-          });
+        })
+            .catch(function (error) {
+                socket.emit('error', error);
+            });
 
     });
 
